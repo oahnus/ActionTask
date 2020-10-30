3481d6c9c438c78cfc56eb7f842a618c153e3843
oahnus <15751774919@163.com>
2020-10-30 19:01:54 +0800

    test

--- src/main/java/com/github/oahnus/travisdemo/MainApp.java
+++ src/main/java/com/github/oahnus/travisdemo/MainApp.java
@@ -9,7 +9,7 @@ import io.itit.itf.okhttp.Response;
 public class MainApp {
     public static void main(String... args) throws Exception {
         String body = String.format("{\"msgtype\": \"markdown\",\"markdown\": {\"title\": \"%s\",\"text\": \"%s\"}}", "ROBOT测试", "测试123");
-        String url = "https://oapi.dingtalk.com/robot/send?access_token=0003dfe75e0a39f007d53d4b793fad65a897f86812f7073d721b4d592cc38d18";
+        String url = "https://oapi.dingtalk.com/robot/send?access_token=ad9a22b93a162e83a52e2fedf2d511f2947bdc1a8702f8d8fb2590d943c39748";
         Response response = new FastHttpClientBuilder().build()
                 .post()
                 .addHeader("Content-Type", "application/json")
